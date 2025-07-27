f68968f7a28327263f31d4710e9c02126d50dbe0
Chrisbin19 <chrisbinsibi19@gmail.com>
2025-07-27 16:26:31 +0530

    Frontend and backend merged

---
+++ backend/src/index.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const app=express();
+const prisma = new PrismaClient();
+app.use(cors());
+app.use(express.json());   
+app.get("/api/notes",async(req,res)=>{
+    res.json({message:"success"});
+});
+app.get("/notes",async(req,res)=>{
+    const notes = await prisma.note.findMany();
+    res.json(notes);
+});
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+app.put("/api/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  const id = parseInt(req.params.id);
+
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send("ID must be a valid number");
+  }
+
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id },
+      data: { title, content },
+    });
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+app.listen(5000,()=>{
+    console.log("Server is running :https://localhost:5000  ");
+});
